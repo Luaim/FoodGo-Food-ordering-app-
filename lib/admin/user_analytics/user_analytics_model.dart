84cc2a8314e3fe2bcb2beba8a2b221f4258f7fe4
ChenYeanMingTakumi <146968962+ChenYeanMingTakumi@users.noreply.github.com>
2024-06-14 15:16:14 +0800

    Update user_analytics_model.dart

--- lib/admin/user_analytics/user_analytics_model.dart
+++ lib/admin/user_analytics/user_analytics_model.dart
@@ -2,8 +2,8 @@ import '/flutter_flow/flutter_flow_data_table.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'user_analytics_widget.dart' show UserAnalyticsWidget;
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'user_analytics_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mae1/common/login_page/user_service.dart';
 
 class UserAnalyticsModel extends FlutterFlowModel<UserAnalyticsWidget> {
   ///  State fields for stateful widgets in this page.
@@ -12,11 +12,30 @@ class UserAnalyticsModel extends FlutterFlowModel<UserAnalyticsWidget> {
   // State field(s) for PaginatedDataTable widget.
   final paginatedDataTableController = FlutterFlowDataTableController<String>();
 
+   // List to store user data
+  List<Map<String, dynamic>> users = [];
+
   @override
-  void initState(BuildContext context) {}
+  void initState(BuildContext context) {
+    fetchUsers();
+  }
 
   @override
   void dispose() {
     unfocusNode.dispose();
+  }
+
+  // Method to fetch inventories from Firestore
+  Future<void> fetchUsers() async {
+    try {
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+      QuerySnapshot snapshot = await firestore.collection('Inventories').get();
+      users = snapshot.docs
+          .map((doc) => doc.data() as Map<String, dynamic>)
+          .toList();
+    } catch (e) {
+      print(e.toString());
+      throw e;
+    }
   }
 }
