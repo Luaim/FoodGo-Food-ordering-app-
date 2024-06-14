746895a6919aaf07fb391b6e8d2e6845a023d3c9
ChenYeanMingTakumi <146968962+ChenYeanMingTakumi@users.noreply.github.com>
2024-06-14 15:15:19 +0800

    Update transactions_model.dart

--- lib/admin/transactions/transactions_model.dart
+++ lib/admin/transactions/transactions_model.dart
@@ -2,19 +2,38 @@ import '/flutter_flow/flutter_flow_data_table.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'transactions_widget.dart' show TransactionsWidget;
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class TransactionsModel extends FlutterFlowModel<TransactionsWidget> {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
-  // State field(s) for PaginatedDataTable widget.
-  final paginatedDataTableController = FlutterFlowDataTableController<double>();
+  late List<Map<String, dynamic>> transactions;
+  late FlutterFlowDataTableController<Map<String, dynamic>> paginatedDataTableController;
+
+  TransactionsModel() {
+    transactions = [];
+    paginatedDataTableController = FlutterFlowDataTableController<Map<String, dynamic>>();
+  }
 
   @override
-  void initState(BuildContext context) {}
+  void initState(BuildContext context) {
+    fetchTransactions();
+  }
 
   @override
   void dispose() {
     unfocusNode.dispose();
+  }
+
+  // Method to fetch transactions from Firestore
+  Future<void> fetchTransactions() async {
+    try {
+      QuerySnapshot snapshot =
+          await FirebaseFirestore.instance.collection('Transactions').get();
+      transactions = snapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
+    } catch (e) {
+      print("Error fetching transactions: $e");
+    }
   }
 }
